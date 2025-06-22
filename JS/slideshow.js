adfccd5da67bf49d95e5948e90ae4d0a6b78c047
Fernando <104527198+CGFernando@users.noreply.github.com>
2025-06-21 23:03:48 -0300

    Update slidesShow
    Improved JavaScript, the JS does not control the CSS anymore, and the slidesShow became more responsive, e.g., the text and image do not overlap

--- JS/slideshow.js
+++ JS/slideshow.js
@@ -18,7 +18,7 @@ function showSlides(n) {
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i].className = slides[i].className.replace(' mySlidesActive', '');
   }
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
@@ -26,6 +26,6 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex-1].className += " mySlidesActive";
   dots[slideIndex-1].className += " active";
 }
